a2a22acae277c2f04c5e2aa13fa3eaaa2aa8ebce
Felopater Sameh <felopaters37@gmail.com>
2025-03-20 02:34:14 +0200

    Organizing Folders Features 2

---
+++ lib/Features/client/App/Main/widgets/body_main_screen.dart
@@ -0,0 +1,1 @@
+import 'package:flutter/material.dart';import 'package:flutter_bloc/flutter_bloc.dart';import '../Cubit/main_cubit.dart';class BodyMainScreen extends StatelessWidget {  const BodyMainScreen({    super.key,    required this.index,  });  final int index;  @override  Widget build(BuildContext context) {    return context.read<MainCubit>().listScreens();  }}
