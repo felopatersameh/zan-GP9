e44dec48508e5db4df35b1dcef528dd9afd4ae39
Felopater Sameh <felopaters37@gmail.com>
2025-05-27 17:09:35 +0300

    remove my key

--- lib/Core/Services/Payment/Strip/data/data_sources/api_keys.dart
+++ lib/Core/Services/Payment/Strip/data/data_sources/api_keys.dart
@@ -4,9 +4,3 @@ class AppEndPoint {
   static const String ephemeral  = 'https://api.stripe.com/v1/ephemeral_keys';
 }
 
-class ApiKeys {
-  static const String secretKey =
-      'sk_test_51PlKFrRoXC95BlOW9gB4ExaC9b6nWxmPlWVEXQOwaydgKwuak2hWOe79XguTlvQU3o5jvkxiL1MLxUkjZqABMZjA00hzMXjaYC';
-  static const String publishKey =
-      'pk_test_51PlKFrRoXC95BlOWr1CNtJCHURmzbDcGhLxpG4KlZnTQTHa6YLHLDIWsZvYH5FTCYckF1TWjOSW9bPnn4WFM0smU00RnwdJ2UC';
-}
